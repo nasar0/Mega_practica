dfc0d9355f160a6f9b4f5c3f32a0a746e846f362
nasrallah <nasaroanas6@gmail.com>
2024-05-15 10:56:55 +0200

    he metido la practica 15/05/2024

---
+++ Pc final/recetario/src/recetario/Basededatos.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recetario;
+import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+/**
+ *
+ * @author EAG
+ */
+public class Basededatos {
+   /*
+    final String url = "jdbc:oracle:thin:@//localhost:1521/xe";
+    final String username = "aula";
+    final String pass = "aula";
+    */
+    final String url = "jdbc:oracle:thin:@//localhost:1521/xe";
+    final String username = "nasaro";
+    final String pass = "1234";
+    
+    private Connection con;
+    
+    public Basededatos() throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        con = DriverManager.getConnection(url, username, pass);
+        System.out.println("Conexión establecida");
+    }
+    public void insertar(String a)throws SQLException {
+        Statement st = con.createStatement();
+        st.executeUpdate("insert into registro values('"+a+"', s ,d,f,f");
+    }
+    public ResultSet consulta(String a) throws SQLException{
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(a);
+        return rs;
+    }
+    public void cerrar() throws SQLException{
+        con.close();
+    }
+    public int inicioSesion(String a,String b)throws SQLException{
+        int num=0;
+        ResultSet nC = consulta("select count(*) from usuarios where usuario='" + a + "' and pass='" + b + "'");
+        nC.next();
+        num = nC.getInt(1);
+        return num;
+    }
+    public int tipoUsuario(String a,String b)throws SQLException{
+        int num=0;
+        ResultSet nC = consulta("select tipo from usuarios where usuario='" + a + "' and pass='" + b + "'");
+        nC.next();
+        num = nC.getInt(1);
+        return num;
+    }
+    public boolean buscarRecetas(String a,boolean b) throws SQLException {
+        int num = 0;
+        try {
+            ResultSet nC = consulta("select count(*) from Recetas where nombre='" + a+"'");
+            nC.next();
+            num = nC.getInt(1);
+        } catch (Exception e) {
+            
+        }
+        if (num == 1) {
+            ResultSet rs = consulta("select * from recetas where nombre='" + a+"'");
+            ResultSet nC = consulta("select count(*) from recetas");
+            nC.next();
+            if (b) {
+                while(rs.next()){
+                for (int i = 2; i <=8; i++) {
+                    String columnName = rs.getMetaData().getColumnName(i);
+                    System.out.println(columnName+": "+rs.getString(i));
+                }
+                skip();
+                }   
+            }            
+            return true;
+        } else {
+            System.out.println("No existe dicha receta. Si no conoce las recetas existentes vaya al apartado de buscar"
+                    + "recetas");
+            return false;
+            
+        }
+    }
+    public void mostrarRecetas(String a)throws SQLException{
+        ResultSet rs = consulta("select * from recetas where nombre='" + a+"'");
+        ResultSet nC = consulta("select count(*) from recetas");
+        nC.next();
+        while(rs.next()){
+            System.out.println(rs.getString(2));
+        }
+        skip();
+    }
+    public void modificarRecetas(String a)throws SQLException{
+        System.out.println("Recetas que puedes modificar");
+        if (a.equals("admin")) {
+            ResultSet rs = consulta("select * from recetas ");
+            ResultSet nC = consulta("select count(*) from recetas");
+            nC.next();
+            while (rs.next()) {
+                System.out.println(rs.getString(2));
+            }
+            skip();
+        }else{
+            mostrarRecetas(a);
+        }
+    }
+    public boolean comprobar(String a)throws SQLException{
+        
+        ResultSet rs = consulta("select * from recetas ");
+        boolean t=false;
+        while (rs.next()) {
+            for (int i = 2; i <= 8; i++) {
+                String columnName = rs.getMetaData().getColumnName(i);
+                if (a.equals(columnName)) {
+                    t=true;
+                }
+            }
+            skip();
+        }
+        return t;
+    
+    }
+    public void skip() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println();
+        }
+    }
+}
---
+++ Pc final/recetario/src/recetario/Recetario.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recetario;
+
+import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+
+
+
+
+/**
+ *
+ * @author EAG
+ */
+public class Recetario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Basededatos bd=new Basededatos();
+        Scanner sc =new Scanner (System.in);
+        int op=0;
+        int t=0;
+        int tipo;
+        
+        String user="";
+        String pass="";
+        String rece="";
+        String modf="";
+        boolean cb=false;
+        boolean a=false;
+        
+        char c='a';
+        
+        /*ResultSet rs = bd.consulta("select * from recetas");
+        ResultSet nC = bd.consulta("select count(*) from recetas");
+        nC.next();
+        while(rs.next()){
+            System.out.println(rs.getString(2));
+        }*/
+        do {
+            System.out.println("1.Iniciar sesion");
+            System.out.println("2.Registrarse");
+            System.out.println("3.Ver recetas");
+            System.out.println("4.Salir");
+            do {
+                try {
+                    op = sc.nextInt();
+                } catch (Exception e) {
+                    System.out.println("Debe elejir uno de los valores que aparecen en pantalla");
+                } finally {
+                    sc.nextLine();
+                }
+            } while (op > 1 && op < 4);
+            switch (op) {
+                case 1:
+                    
+                        do {
+                            try {
+                                System.out.print("Usuario: ");
+                                user = sc.nextLine();
+                                System.out.print("Contraseña: ");
+                                pass = sc.nextLine();
+                                t = bd.inicioSesion(user, pass);
+                            } catch (Exception e) {
+                                System.out.println("Usuario o contraseña incorrectos");
+                            }
+                        } while (t != 1);
+                        tipo = bd.tipoUsuario(user, pass);
+                        do {
+                        if (tipo == 1) {
+                            System.out.println("1.Buscar recetas");
+                            System.out.println("2.Modificar recetas");
+                            System.out.println("3.Ver recetas");
+                            System.out.println("4.Ver usuarios");
+                            System.out.println("5.Cerrar sesion");
+                            op=0;
+                            do {
+                                try {
+                                    op = sc.nextInt();
+                                } catch (Exception e) {
+                                    System.out.println("Debe elegir uno de los valores que aparecen en pantalla");
+                                } finally {
+                                    sc.nextLine();
+                                }
+                            } while (op < 1 && op > 5);
+                            
+                            switch (op) {
+                                case 1:
+                                    do {
+                                        System.out.println("Dime la recetas que quieres buscar: ");
+                                        rece = sc.nextLine();
+                                        rece = rece.split("\n")[0];
+                                        sc.nextLine();
+                                        System.out.println("Quieres ver la informacion de la receta¿?(Si/No)");
+                                        c=sc.next().toLowerCase().charAt(0);
+                                        if (c=='s') {
+                                            cb=true;
+                                        }else if (c=='n'){
+                                            cb=false;
+                                        }
+                                        a = bd.buscarRecetas(rece,cb);
+                                    } while (!a);
+
+                                    break;
+                                case 2:
+                                        bd.mostrarRecetas(user);
+                                        System.out.println("Cual de sea modificar ");
+                                        rece = sc.nextLine();
+                                        cb=false;
+                                        a = bd.buscarRecetas(rece,cb);
+                                        if (a) {
+                                            System.out.print("Que desea modificar: ");
+                                            modf=sc.nextLine();
+                                            
+                                        }else{
+                                            System.out.println("No encontrada");
+                                        }
+                                            
+                                    break;
+                                case 3:
+                                    cb=false;
+                                    bd.mostrarRecetas(user);
+                                    break;
+                                case 4:
+
+                                    break;
+                                case 5:
+                                    System.out.println("Cerrando sesion......");
+                                    break;
+                            }
+                        } else if (tipo == 2) {
+
+                        }
+                    } while (op!=5);
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Hasta pronto <<<<<<3");
+                    break;
+                default:
+                    break;
+            }
+
+        } while (op!=4);
+        
+    } 
+   
+
+}
